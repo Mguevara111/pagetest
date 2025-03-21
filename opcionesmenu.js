5f7bf4657f47e18910e69558519f07ab9cabb444
Mguevara111 <marcelo.guevara1983@gmail.com>
2025-03-21 18:15:52 -0500

    Add files via upload

--- opcionesmenu.js
+++ opcionesmenu.js
@@ -1,5 +1,6 @@
 export const menu=[
 {
+    id:'1',
     nombre:"Margarita",
     tamanio:["personal","pequeño","mediano","grande","gigante"],
     caracteristicas:"pizza con salsa de tomate y queso",
@@ -7,6 +8,7 @@ export const menu=[
     foto:`imagenes/salame.jpg`
 },
 {
+    id:'2',
     nombre:"Napolitana",
     tamanio:["personal","pequeño","mediano"],
     caracteristicas:"pizza con salsa de tomate y queso y albahaca",
@@ -14,6 +16,7 @@ export const menu=[
     foto:`imagenes/napolitana.jpg`
 },
 {
+    id:'3',
     nombre:"Salame",
     tamanio:["personal","pequeño","mediano","grande","gigante"],
     caracteristicas:"pizza con salame, queso mozzarella y jamon",
@@ -21,6 +24,7 @@ export const menu=[
     foto:`imagenes/salame.jpg`
 },
 {
+    id:'4',
     nombre:"Jamon",
     tamanio:["mediano","grande","gigante"],
     caracteristicas:"pizza con jamon y queso mozzarella",
@@ -28,6 +32,7 @@ export const menu=[
     foto:`imagenes/margarita.jpg`
 },
 {
+    id:'5',
     nombre:"Pollo",
     tamanio:["personal","pequeño","mediano","grande","gigante"],
     caracteristicas:"pizza con pollo, jamon y queso",
@@ -35,28 +40,32 @@ export const menu=[
     foto:`imagenes/napolitana.jpg`
 },
 {
+    id:'6',
     nombre:"Alemana",
     tamanio:["mediano","grande","gigante"],
     caracteristicas:"pizza con salchicha alemana y salsa brewer",
     costo:["$14","$17","$23"],
-    foto:`imagenes/salame.jpg`
+    foto:`imagenes/margarita.jpg`
 },
 {
+    id:'7',
     nombre:"Costilla",
     tamanio:["personal","pequeño","mediano","grande","gigante"],
     caracteristicas:"pizza con costilla de cerdo ahumada",
     costo:["$5","$10.50","$14","$17","$23"],
-    foto:`imagenes/margarita.jpg`
-},
-{
-    nombre:"Tocino_chancho",
-    tamanio:["grande","gigante"],
-    caracteristicas:"pizza con tocino ahumado y salsa",
-    costo:["$17","$23"],
     foto:`imagenes/napolitana.jpg`
 },
 {
-    nombre:"4_quesos",
+    id:'8',
+    nombre:"Tocino",
+    tamanio:["grande","gigante"],
+    caracteristicas:"pizza con tocino ahumado y salsa",
+    costo:["$17","$23"],
+    foto:`imagenes/salame.jpg`
+},
+{
+    id:'9',
+    nombre:"4 quesos",
     tamanio:["personal","pequeño","mediano","grande","gigante"],
     caracteristicas:"pizza con fontine,mozarella,provolone y parmesano",
     costo:["$5","$10.50","$14","$17","$23"],
--- restaurante.js
+++ restaurante.js
@@ -7,6 +7,12 @@ let $borrascript=null;//borra el script para quye no se cree cada vez que se lo 
 let $modal=document.getElementById('exampleModal');
 let $modalbody=document.querySelector('.modal-body');
 let $modalheader=document.querySelector('.modal-header');
+
+let letrasvermenu1=document.querySelector('.vermenu');
+let contact=document.querySelector('.contact');
+let botondianoche=document.querySelector('.ndbutton');
+let darkmode=document.querySelector('[data-dark]');
+let navdarkmode=document.querySelector('[data-navdark]');
 // function generamenu(){
     
 //     menu.forEach(el=>{
@@ -37,6 +43,9 @@ const cargapagina= async function(pagina){
         }else{
             $main.innerHTML=`${datos}`;
             if (pagina==='menu'){
+                
+            
+                
             let $script=document.createElement('script');
             $script.type='module';
             $script.id='scriptmenu';
@@ -44,19 +53,20 @@ const cargapagina= async function(pagina){
             import {menu} from "./opcionesmenu.js";
             let $fragmento=document.createDocumentFragment();
             let $productosmenu=document.getElementById('productosmenu');
+            
+
             menu.forEach(el => {
             let $divmenu = document.createElement('div');
             
             $divmenu.classList.add('col-12','col-lg-4','text-center','mb-0','mb-2')
             $divmenu.innerHTML = \`
                 <a href="" type="button" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#exampleModal" >
-                <figure class="figure border border-2 border-dark" data-nombre=\` + el.nombre + \`>
-                    <img src="imagenes/napolitana.jpg" alt="" class="figure-img img-fluid">
-                    <figcaption class="figure-caption ps-1">
-                        <strong>Nombre: \` + el.nombre + \`</strong> <br>
-                        Tamaño: \` + el.tamanio + \` <br>
-                        Caracteristicas: \` + el.caracteristicas + \` <br>
-                        Costo: \` + el.costo + \`
+                <figure class="figure border border-2 border-dark pb-3 figuremenu" data-id=\` + el.id + \`>
+                    <img src="\` + el.foto +\`" alt="" class="figure-img img-fluid">
+                    <figcaption class="figure-caption ps-0 fc">
+                        <div class="divfigureg"></div>
+                        <strong>\` + el.nombre + \`</strong> <br>
+                        <div class="divfigurer"></div>
                     </figcaption>
                 </figure>
                 </a>
@@ -64,11 +74,30 @@ const cargapagina= async function(pagina){
             $fragmento.appendChild($divmenu);
             });
         $productosmenu.appendChild($fragmento);
+        
+        const elementosmenug=document.querySelectorAll('.divfigureg');
+        const elementosmenur=document.querySelectorAll('.divfigurer');
+            const cb=(entries)=>{
+                entries.forEach(ent=>{
+                    if(ent.isIntersecting){
+                        ent.target.classList.add('animarletrasmenu')
+                        
+                    }else{
+                       ent.target.classList.remove('animarletrasmenu') 
+                    }
+                    })
+                }
+            const ob=new IntersectionObserver(cb,{threshold:1})
+            elementosmenur.forEach(el=>ob.observe(el));
+            elementosmenug.forEach(el=>ob.observe(el));
+
         `;
         
         $body.appendChild($script);
         } 
         $borrascript=document.getElementById('scriptmenu');//borra el script para quye no se cree cada vez que se lo llama
+        
+            
     }
     } catch (error) {
         console.log(`hubo un error ${error.err}`);
@@ -77,39 +106,96 @@ const cargapagina= async function(pagina){
 
 
 document.addEventListener('click',(e)=>{
-    console.log(e.target);
+    //console.log(e.target);
     
    if (e.target.dataset.nom==='menu'){
    // console.log(e.target)
     cargapagina('menu');
    }
-   if(e.target.dataset.nom==='reservaciones'){
-    cargapagina('reservaciones');
-    $borrascript.remove();   //borra el script para quye no se cree cada vez que se lo llama
+//    if(e.target.dataset.nom==='reservaciones'){
+//     cargapagina('reservaciones');
+//     $borrascript.remove();   //borra el script para quye no se cree cada vez que se lo llama
     
-   }
+//    }
    if(e.target.dataset.nom==='principal'){
     location.reload();
    }
-   if(e.target.parentNode.dataset.nombre){
-    //console.log(menu[0].nombre);
-    const a=menu.find(el=>el.nombre===e.target.parentNode.dataset.nombre)
+   if(e.target.parentNode.dataset.id){
+    console.log(e.target.parentNode.dataset.id);
+    const a=menu.find(el=>el.id===e.target.parentNode.dataset.id)
     //****************************los nombres no deben tener espacio(piiza quesos), mejor usar id en la base */
-    //console.log(Object.values(a));
+    //console.log(a);
     $modalheader.innerHTML=`<h1>${a.nombre}</h1><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>`;
     $modalbody.innerHTML=`
     <figure class="figure"> 
         <img src=${a.foto} alt="" class="figure-img img-fluid">
                     <figcaption class="figure-caption ps-1">
-                        <strong>Nombre: ${a.nombre}</strong> <br>
-                        Tamaño:${a.tamanio}<br>
-                        Caracteristicas:${a.caracteristicas}<br>
-                        Costo: ${a.costo}
+                        ${a.caracteristicas}<br>
+                        Costo: ${a.costo[0]}
                     </figcaption>
     </figure>
     `;
     
     //console.log(e.target.parentNode.dataset.nombre)
    }
+   if(e.target.matches('.vermenu')){
+    
+    cargapagina('menu');
+   }
+   if(e.target.matches('.ndbutton')){
+    //console.log('dia/noche')
+    let sol='🔆'
+    let luna='🌙'
+    if(e.target.textContent===luna){
+        $body.classList.add('dark')
+        darkmode.classList.remove('link-dark')
+        darkmode.classList.add('link-light')
+        navdarkmode.classList.remove('navbar-light','bg-light')
+        navdarkmode.classList.add('navbar-dark','bg-dark')
+        //console.log('sol')
+        botondianoche.textContent='🔆'
+    }else if(e.target.textContent===sol){
+        $body.classList.remove('dark')
+        darkmode.classList.add('link-dark')
+        darkmode.classList.remove('link-light')
+        navdarkmode.classList.add('navbar-light','bg-light')
+        navdarkmode.classList.remove('navbar-dark','bg-dark')
+        //console.log('luna')
+        botondianoche.textContent='🌙'
+    }
+    
+   }
 })
 
+const entradas=(c,e)=>{
+    const cb1=(entries)=>{
+        entries.forEach(ent=>{
+            if(ent.isIntersecting){
+                ent.target.classList.add(c)
+                
+            }else{
+               ent.target.classList.remove(c) 
+            }
+            })
+        }
+    const obser=new IntersectionObserver(cb1,{threshold:1})
+    obser.observe(e);
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    entradas('imgcontact',contact);
+    //entradas('letrasvermenu',letrasvermenu1);
+    //console.log(letrasvermenu1)
+    const cb2=(entries)=>{
+        entries.forEach(ent=>{
+            if(ent.isIntersecting){
+                ent.target.classList.add('letrasvermenu')
+                
+            }
+            })
+        }
+    const obser1=new IntersectionObserver(cb2,{threshold:1})
+    obser1.observe(letrasvermenu1);   
+    
+
+})
